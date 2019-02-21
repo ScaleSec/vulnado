10857dcc28adc6b117849cb34edb269e26a17f47
Ryan <ryan@scalesec.com>
2019-02-20 18:01:15 -0800

    Added supplementary ssrf endpoint

--- src/main/java/com/scalesec/vulnado/LinksController.java
+++ src/main/java/com/scalesec/vulnado/LinksController.java
@@ -6,13 +6,18 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.autoconfigure.*;
 import java.util.List;
 import java.io.Serializable;
+import java.io.IOException;
+
 
 @RestController
 @EnableAutoConfiguration
 public class LinksController {
-    @CrossOrigin(origins = "*")
-    @RequestMapping(value = "/links", produces = "application/json")
-    List<String> links(@RequestParam String url) {
-        return LinkLister.getLinks(url);
-    }
+  @RequestMapping(value = "/links", produces = "application/json")
+  List<String> links(@RequestParam String url) throws IOException{
+    return LinkLister.getLinks(url);
+  }
+  @RequestMapping(value = "/links-v2", produces = "application/json")
+  List<String> linksV2(@RequestParam String url) throws BadRequest{
+    return LinkLister.getLinksV2(url);
+  }
 }
